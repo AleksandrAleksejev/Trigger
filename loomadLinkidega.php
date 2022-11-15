719e76add7cfc8c62a606698b3c1c43ceeea863f
AleksandrAleksejev <93324220+AleksandrAleksejev@users.noreply.github.com>
2022-11-15 11:41:51 +0200

    Update loomadLinkidega.php

--- loomadLinkidega.php
+++ loomadLinkidega.php
@@ -1,95 +1,59 @@
-<?php
-require_once('connect.php');
-global $yhendus;
-if(isset($_REQUEST["kustuta"])){
-    $kask=$yhendus->prepare("DELETE FROM loomad WHERE id=?");
-    $kask->bind_param("i", $_REQUEST["kustuta"]);
-    $kask->execute();
-}
-// andmete lisamine tabelisse
-if(isset($_REQUEST['lisamisvorm']) && !empty($_REQUEST["nimi"])){
-    $paring=$yhendus->prepare(
-        "INSERT INTO loomad(loomanimi, vanus, pilt) Values (?,?,?)"
-    );
-    $paring->bind_param("sis", $_REQUEST["nimi"], $_REQUEST["vanus"], $_REQUEST["pilt"]);
-    //"s" - string, $_REQUEST["nimi"] - tekstkasti nimega nimi pöördumine
-    //sdi, s-string, d-double, i-integer
     $paring->execute();
-    //aadressi ribas eemaldatakse php käsk
-    header("Location: $_SERVER[PHP_SELF]");
-
 }
-// kustutamine tabelist
 
-
-
+$paring=$yhendus->prepare('SELECT raamatuID, raamatuNimi,kirjutamiseAasta,autor FROM loomadevarjupaik');
+$paring->bind_result($raamatuID, $raamatuNimi, $kirjutamiseAasta,$autor);
+$paring->execute();
 ?>
+
 <!DOCTYPE html>
 <html lang="et">
 <head>
     <meta charset="UTF-8">
-    <title>Ramaatu</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Raamatukogu</title>
 </head>
 <body>
-<h1>Ramaatu</h1>
-<div id="meny">
-    <ul>
+
+<h1>Loomade tabel</h1>
+<table>
+    <tr>
+        <th>RaamatuID</th>
+        <th>Raamatu nimi</th>
+        <th>kirjutamiseAasta</th>
+        <th>autor</th>
+        <th>Kustuta</th>
+    </tr>
     <?php
-    //näitab loomade loetelu tabelist loomad
-    $paring=$yhendus->prepare("SELECT id, nimi FROM Ramaatu");
-    $paring->bind_result($id, $nimi);
-    $paring->execute();
+    while($paring->fetch()){
+        echo "<tr>";
+        echo "<td>". htmlspecialchars($raamatuID)."</td>";
+        echo "<td>". htmlspecialchars($raamatuNimi)."</td>";
+        echo "<td>". htmlspecialchars($kirjutamiseAasta)."</td>";
+        echo "<td>". htmlspecialchars($autor)."</td>";
 
-        while($paring->fetch()) {
-        echo "<li><a href='?id=$id'>$nimi</a></li>";
-        }
-        echo "</ul>";
-        echo "<a href='?lisaloom=jah'>Lisa Loom</a>";
-    ?>
-
-</div>
-<div id="sisu">
-
-    <?php
-    if(isset($_REQUEST["id"])){
-        $paring=$yhendus->prepare("
-SELECT nimi, nimi2, dat,FROM Ramaatu WHERE id=?");
-        $paring->bind_param("i", $_REQUEST["id"]);
-        //? küsimärki asemel aadressiribalt tuleb id
-        $paring->bind_result($nimi, $nimi2, $dat);
-        $paring->execute();
-        if($paring->fetch()){
-            echo "<div><strong>".htmlspecialchars($nimi)."</strong>, vanus ";
-            echo htmlspecialchars(nimi2). "";
-            echo htmlspecialchars(dat). " date.";
-            echo "</div>";
-
-        }
-        echo "<a href='?kustuta=$id'>Kustuta</a>";
+        echo "<td><a href='$_SERVER[PHP_SELF]?kustutusid=$raamatuID'>kustuta</a></td>";
+        echo "</tr>";
     }
-
-
-        if(isset($_REQUEST["lisaraamat"])){
-            ?>
-    <h2>Uue looma lisamine</h2>
-    <form name="uusloom" method="post" action="?">
-    <input type="hidden" name="lisamisvorm" value="jah">
-    <input type="text" name="nimi" placeholder="Raamat nimi">
+    ?>
+</table>
+<h2>Uue raamat lisamine</h2>
+<form name="uusloom" method="post" action="?">
+    <input type="hidden" name="lisamisvorm">
+    <input type="text" name="raamatuNimi" placeholder="raamatu Nimi">
     <br>
-    <input type="text" name="nimi"  max="30" placeholder="Autor">
     <br>
-    <input type="number" name="date"  max="30" placeholder="kirjutamiseAasta">
+    <input type="text" name="kirjutamiseAasta" placeholder="kirjutamise Aasta">
+    <br>
+    <br>
+    <input type="text" name="autor" placeholder="autor">
+    <br>
     <br>
     <input type="submit" value="OK">
-    </form>
-<?php
-        }
-        else {
-            echo " <h3>Siia tuleb loomade info...</h3>";
-        }
-    $yhendus->close();
-    ?>
-</div>
+</form>
 </body>
+<?php
+$yhendus->close();
+?>
 </html>
+
+
